b568d316633b8ce34904703dabd91599195f118e
GrophBanks <jbeck08@gmail.com>
2019-03-15 23:30:17 -0600

    controller actions add and search working

--- PeopleSearch/Global.asax.cs
+++ PeopleSearch/Global.asax.cs
@@ -12,6 +12,8 @@ namespace PeopleSearch
         protected void Application_Start()
         {
             GlobalConfiguration.Configure(WebApiConfig.Register);
+
+            AutoMapperConfiguration.Configure();
         }
     }
 }
