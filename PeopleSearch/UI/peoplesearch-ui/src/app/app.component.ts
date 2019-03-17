bff557b6dd1412e3627de05a4d869ae80c99f33f
GrophBanks <jbeck08@gmail.com>
2019-03-16 20:10:33 -0600

    Angular communicating with web api. Seach and add both working

--- PeopleSearch/UI/peoplesearch-ui/src/app/app.component.ts
+++ PeopleSearch/UI/peoplesearch-ui/src/app/app.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, } from '@angular/core';
+import { HttpService } from './services/http.service';
+import { Person} from './models/person';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +9,49 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'peoplesearch-ui';
+
+  searchString = new FormControl('');
+
+  personForm = new FormGroup({
+    firstName: new FormControl(''),
+    lastName: new FormControl(''),
+    address: new FormControl(''),
+    interests: new FormControl(''),
+  });
+
+  addPersonResponseMessage: string;
+  newPerson:string;
+
+  constructor(private httpService: HttpService){
+    
+  };
+
+  addPerson(): void{
+    
+    this.addPersonResponseMessage = '';
+
+    let serializedForm = JSON.stringify(this.personForm.getRawValue());
+
+    this.httpService.Post("Search", "Add",  serializedForm)
+      .subscribe(response => this.searchResponse(response))
+  }
+
+  search(): void{
+
+    this.httpService.Get("Search", undefined, this.searchString.value)
+      .subscribe(response => this.searchResponse(response))
+  }
+
+  searchResponse(searchResponse: any): void{
+     console.log(JSON.stringify(searchResponse));
+  }
+
+  addPersonResponse(response: any): void {
+    if(response.PersonId){
+      this.addPersonResponseMessage = "Person Added Successfully!"
+      this.newPerson = response;
+    }
+
+    console.log(JSON.stringify(response));
+  }
 }
--- PeopleSearch/UI/peoplesearch-ui/src/app/app.module.ts
+++ PeopleSearch/UI/peoplesearch-ui/src/app/app.module.ts
@@ -1,8 +1,11 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+
+import { HttpService } from './services/http.service';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -10,9 +13,12 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    FormsModule,    
+    AppRoutingModule,
+    HttpClientModule,
+    ReactiveFormsModule
   ],
-  providers: [],
+  providers: [HttpService, HttpClient],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
